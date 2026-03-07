6c99e880975042830b0fd9b217bfd6d600ccd9f7
YutoImata <yuto.imata.wine@gmail.com>
2026-03-07 20:10:37 +0900

    fix: iOS Safari対応の背景画像表示に変更、透明度を調整

--- 5_Sisters/service-worker.js
+++ 5_Sisters/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gotoubun-shiori-v20260307-6';
+const CACHE_NAME = 'gotoubun-shiori-v20260307-9';
 const urlsToCache = [
   './html/quintuplets.html',
   './html/quintuplets_home.html',
