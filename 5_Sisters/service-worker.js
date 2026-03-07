c20d25e9115833d9922d6667913b0e0cc1741cc7
YutoImata <yuto.imata.wine@gmail.com>
2026-03-07 20:19:49 +0900

    feat: 最背面に超薄い背景画像レイヤーを追加

--- 5_Sisters/service-worker.js
+++ 5_Sisters/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gotoubun-shiori-v20260307-12';
+const CACHE_NAME = 'gotoubun-shiori-v20260307-13';
 const urlsToCache = [
   './html/quintuplets.html',
   './html/quintuplets_home.html',
