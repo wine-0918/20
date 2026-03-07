7b396920059bc0d8aae15a76766a543ec95f1e1d
YutoImata <yuto.imata.wine@gmail.com>
2026-03-07 20:21:09 +0900

    fix: 端末差で消える背景レイヤーを安定表示へ変更

--- 5_Sisters/service-worker.js
+++ 5_Sisters/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gotoubun-shiori-v20260307-13';
+const CACHE_NAME = 'gotoubun-shiori-v20260307-14';
 const urlsToCache = [
   './html/quintuplets.html',
   './html/quintuplets_home.html',
